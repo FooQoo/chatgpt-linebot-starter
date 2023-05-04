73e86eb443669a3fc06b9a6ac71ce43dbdc50cc6
FooQoo <horseshoes.buzz.17@gmail.com>
2023-05-04 11:35:31 +0900

    first commit

---
+++ src/handler.ts
@@ -0,0 +1,59 @@
+import { middleware, MiddlewareConfig, WebhookEvent } from '@line/bot-sdk';
+import serverlessExpress from '@vendia/serverless-express';
+import express, { Request, Response } from 'express';
+import * as sourceMapSupport from 'source-map-support';
+
+import { textEventHandler } from './lib/line/textEventHandler';
+
+sourceMapSupport.install();
+
+const app = express();
+
+// middleware
+const middlewareConfig: MiddlewareConfig = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET || '',
+};
+
+const basePath = '/api';
+
+// This route is used for the Webhook.
+app.post(
+  `${basePath}/webhook`,
+  middleware(middlewareConfig),
+  async (req: Request, res: Response): Promise<Response> => {
+    const events: WebhookEvent[] = req.body.events;
+
+    // Process all of the received events asynchronously.
+    const results = await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        try {
+          await textEventHandler(event);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            console.error(err);
+          }
+
+          // Return an error message.
+          return res.status(500).json({
+            status: 'error',
+          });
+        }
+      })
+    );
+
+    // Return a successfull message.
+    return res.status(200).json({
+      status: 'success',
+      results,
+    });
+  }
+);
+
+app.use((_req, res) => {
+  return res.status(404).json({
+    error: 'Not Found',
+  });
+});
+
+export const handler = serverlessExpress({ app });
---
+++ src/lib/langchain/chat.ts
@@ -0,0 +1,49 @@
+import crypto from 'crypto';
+import { LLMChain } from 'langchain';
+import { OpenAI } from 'langchain/llms/openai';
+import { PromptTemplate } from 'langchain/prompts';
+
+const prompt = `以下の文章に回答してください。なお、回答の最初に「{hashValue}」を復唱してから翻訳してください。
+{userInput}`;
+
+const model = new OpenAI();
+
+export const chat = async (message: string): Promise<string> => {
+  // ランダムな16バイトのソルトを生成する
+  const salt = crypto.randomBytes(16).toString('hex');
+
+  // パスワードとソルトを結合する
+  const saltedMessage = message + salt;
+
+  // SHA256ハッシュ関数を使用して、ソルトとパスワードを混ぜ合わせてハッシュ化する
+  const hashedMessage = crypto
+    .createHash('sha256')
+    .update(saltedMessage)
+    .digest('hex');
+
+  const template = new PromptTemplate({
+    inputVariables: ['userInput', 'hashValue'],
+    template: prompt,
+  });
+
+  const chain = new LLMChain({
+    prompt: template,
+    llm: model,
+  });
+
+  const response = await chain.call({
+    userInput: message,
+    hashValue: hashedMessage,
+  });
+
+  const text: string = response.text;
+
+  console.info('text:', text);
+  console.info('hash:', hashedMessage);
+
+  if (text.includes(hashedMessage)) {
+    return text.replace(hashedMessage, '').trim();
+  } else {
+    return 'Unable to recognize. Please try again.';
+  }
+};
